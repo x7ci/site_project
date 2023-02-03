39c4ff7d678e46da08b2ef071960db13634bd2fa
x7ci <122831534+x7ci@users.noreply.github.com>
2023-02-03 23:27:46 +0100

    improve table component

--- src/modules/index/DetailedMetrics/TableData2.tsx
+++ src/modules/index/DetailedMetrics/TableData2.tsx
@@ -4,13 +4,14 @@ import useRandomRange from "@/helpers/hooks/useRandomRange";
 import { styled } from "stitches.config";
 import { Text } from '../../../components/stitches/Text';
 
-const TableData = () => {
-    const coreTempAvg = useRandomRange({ minValue: 150, maxValue: 190, updateInterval: 5000 });
-    const coreThreAvg = useRandomRange({ minValue: 25, maxValue: 50, updateInterval: 5000 });
-    const coreLoadMax = useRandomRange({ minValue: 73, maxValue: 97, updateInterval: 5000 });
-    const cacheLat = useRandomRange({ minValue: 40, maxValue: 70, updateInterval: 5000, digits: 0 });
-    const dramLat = useRandomRange({ minValue: 40, maxValue: 70, updateInterval: 5000, digits: 0 });
-    const waterPumpSp = useRandomRange({ minValue: 620, maxValue: 800, updateInterval: 5000 });
+const TableData2 = () => {
+    const outputRange = useRandomRange({ minValue: 1100, maxValue: 1400, updateInterval: 5000 });
+    const steamPressure = useRandomRange({ minValue: 60, maxValue: 75, updateInterval: 5000 });
+    const steamTemp = useRandomRange({ minValue: 250, maxValue: 300, updateInterval: 5000 });
+    const turbineSpeed = useRandomRange({ minValue: 5122, maxValue: 10000, updateInterval: 5000 });
+    const turbineHotspot = useRandomRange({ minValue: 80, maxValue: 90, updateInterval: 5000, digits: 1 });
+    // const turbine = useRandomRange({ minValue: 40, maxValue: 70, updateInterval: 5000, digits: 1 });
+    const mainBearingTemp = useRandomRange({ minValue: 40, maxValue: 60, updateInterval: 5000 });
     const yawAxisDeg = useRandomRange({ minValue: 14, maxValue: 25, updateInterval: 5000 });
     const yawAxisVel = useRandomRange({ minValue: 50, maxValue: 60, updateInterval: 5000 });
     const currOutput = useRandomRange({ minValue: 100, maxValue: 126, updateInterval: 5000 });
@@ -23,76 +24,65 @@ const TableData = () => {
                     <TableBody>
                         <TableRow>
                             <TableCell>
-                                <Text size="4" color="gray10">CORE_TEMP_AVG</Text>
+                                <Text size="4" color="gray10">TOTAL_THREADS</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="cyan">{coreTempAvg}</Text>
+                                <Text size="4" color="cyan">32</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="gray10">°F</Text>
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <Text size="4" color="gray10">CORE_THRE_AVG</Text>
-                            </TableCell>
-                            <TableCell>
-                                <Text size="4" color="cyan">{coreThreAvg}</Text>
-                            </TableCell>
-                            <TableCell>
-                                <Text size="4" color="gray10">%</Text>
+                                <Text size="4" color="gray10">--</Text>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>
-                                <Text size="4" color="gray10">CORE_LOAD_MAX</Text>
+                                <Text size="4" color="gray10">FREQ_GOVERNOR</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="cyan">{coreLoadMax}</Text>
+                                <Text size="4" color="cyan">convervative</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="gray10">%</Text>
+                                <Text size="4" color="gray10">--</Text>
+                            </TableCell>
+                        </TableRow>
+                        {/* <TableRow>
+                            <TableCell>
+                                <Text size="4" color="gray10">IO_SCHED</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="cyan">mq-deadline</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="gray10">--</Text>
+                            </TableCell>
+                        </TableRow> */}
+                        <TableRow>
+                            <TableCell>
+                                <Text size="4" color="gray10">OUTPUT_RANGE</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="cyan">{outputRange}</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="gray10">mw</Text>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>
-                                <Text size="4" color="gray10">CACHE_LAT</Text>
+                                <Text size="4" color="gray10">STEAM_PRESSURE</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="cyan">{cacheLat}</Text>
+                                <Text size="4" color="orange1">{steamPressure}</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="gray10">ms</Text>
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <Text size="4" color="gray10">DRAM_LAT</Text>
-                            </TableCell>
-                            <TableCell>
-                                <Text size="4" color="cyan">{dramLat}</Text>
-                            </TableCell>
-                            <TableCell>
-                                <Text size="4" color="gray10">ms</Text>
+                                <Text size="4" color="gray10">bar</Text>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>
-                                <Text size="4" color="gray10">WATER_PUMP_SP</Text>
+                                <Text size="4" color="gray10">STEAM_TEMP</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="cyan">{waterPumpSp}</Text>
-                            </TableCell>
-                            <TableCell>
-                                <Text size="4" color="gray10">rpm</Text>
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <Text size="4" color="gray10">YAW_AXIS_DEG</Text>
-                            </TableCell>
-                            <TableCell>
-                                <Text size="4" color="cyan">{yawAxisDeg}</Text>
+                                <Text size="4" color="cyan">{steamTemp}</Text>
                             </TableCell>
                             <TableCell>
                                 <Text size="4" color="gray10">°c</Text>
@@ -100,10 +90,43 @@ const TableData = () => {
                         </TableRow>
                         <TableRow>
                             <TableCell>
-                                <Text size="4" color="gray10">YAW_AXIS_VEL</Text>
+                                <Text size="4" color="gray10">TURBINE_SPEED</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="cyan">{yawAxisVel}</Text>
+                                <Text size="4" color="cyan">{turbineSpeed}</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="gray10">rpm</Text>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <Text size="4" color="gray10">TURBINE_VIBR</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="cyan">41</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="gray10">hz</Text>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <Text size="4" color="gray10">TURBINE_HOTSPOT</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="orange1">{turbineHotspot}</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="gray10">°c</Text>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <Text size="4" color="gray10">MAIN_BEARING_TEMP</Text>
+                            </TableCell>
+                            <TableCell>
+                                <Text size="4" color="cyan">{mainBearingTemp}</Text>
                             </TableCell>
                             <TableCell>
                                 <Text size="4" color="gray10">deg/s</Text>
@@ -111,24 +134,24 @@ const TableData = () => {
                         </TableRow>
                         <TableRow>
                             <TableCell>
-                                <Text size="4" color="gray10">CURR_OUTPUT</Text>
+                                <Text size="4" color="gray10">TOTAL_PWR</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="orange1">{currOutput}</Text>
+                                <Text size="4" color="cyan">13931</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="gray10">w</Text>
+                                <Text size="4" color="gray10">mWh</Text>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>
-                                <Text size="4" color="gray10">MAX_OUTPUT</Text>
+                                <Text size="4" color="gray10">UPTIME</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="cyan">130</Text>
+                                <Text size="4" color="cyan">1</Text>
                             </TableCell>
                             <TableCell>
-                                <Text size="4" color="gray10">w</Text>
+                                <Text size="4" color="gray10">days</Text>
                             </TableCell>
                         </TableRow>
                     </TableBody>
@@ -152,4 +175,4 @@ const TableBodyWrapper = styled('div', {
     // maxHeight: 210, // Enables scroll
 });
 
-export default TableData;
+export default TableData2;
