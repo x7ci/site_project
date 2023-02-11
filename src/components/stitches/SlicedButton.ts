cee35bc80b1341f7ee0eb808efbea5a42a02c22c
x7ci <122831534+x7ci@users.noreply.github.com>
2023-02-11 16:40:12 +0100

    improve SlicedButton design

--- src/components/stitches/SlicedButton.ts
+++ src/components/stitches/SlicedButton.ts
@@ -1,28 +1,17 @@
 import { styled } from 'stitches.config';
 
 export const SlicedButton = styled('button', {
-  weight: {
-    1: {
-      fontWeight: '300'
-    },
-    2: {
-      fontWeight: '400'
-    },
-    3: {
-      fontWeight: '700'
-    },
-  },
   border: 0,
-  padding: '6px 32px 6px 32px',
+  padding: '4px 32px 4px 32px',
   position: 'relative',
   background: 'transparent',
   cursor: 'pointer',
   fontFamily: 'inherit',
   fontSize: '$fontSizes$6',
-  transition: '200ms',
+  transition: 'all 200ms ease-out',
   zIndex: 1,
   whiteSpace: 'nowrap',
-  color: '$gray12',
+  color: '$cyan1',
   '&:before': {
     content: '',
     position: 'absolute',
@@ -35,21 +24,110 @@ export const SlicedButton = styled('button', {
     // backgroundColor: 'rgba(0, 0, 0, 0.3)',
     backgroundColor: '$navigationBarBackground',
     zIndex: -1,
-    '@bp3': {
-      transform: 'unset'
-    },
   },
   '&:hover': {
     color: '$yellow1',
     '&:before': {
       transition: '250ms',
-      backgroundColor: 'rgba(0, 0, 0, 0.4)',
+      backgroundColor: '$cyan10',
+      // backgroundColor: 'rgba(0, 0, 0, 0.4)',
     }
   },
   '&:active': {
     color: '$yellow1',
     '&:before': {
-      backgroundColor: 'rgba(0, 0, 0, 0.0)',
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
     }
-  }
+  },
+  transform: 'unset',
+  variants: {
+    variant: {
+      standard: {
+        '&:before': {
+          transform: 'unset'
+        },
+      },
+    },
+    color: {
+      cyan1: {
+        color: '$cyan1',
+      },
+      cyan2: {
+        color: '$cyan2',
+      },
+      cyan3: {
+        color: '$cyan3',
+      },
+      cyan4: {
+        color: '$cyan4',
+      },
+      cyan5: {
+        color: '$cyan5',
+      },
+      cyan6: {
+        color: '$cyan6',
+      },
+      cyan7: {
+        color: '$cyan7',
+      },
+      cyan8: {
+        color: '$cyan8',
+      },
+      cyan9: {
+        color: '$cyan9',
+      },
+      cyan14: {
+        color: '$cyan14',
+      },
+    },
+    fontSize: {
+      1: {
+        fontSize: '$1',
+      },
+      2: {
+        fontSize: '$2',
+      },
+      3: {
+        fontSize: '$3',
+      },
+      4: {
+        fontSize: '$4',
+      },
+      5: {
+        fontSize: '$5',
+      },
+      6: {
+        fontSize: '$6',
+      },
+      7: {
+        fontSize: '$7',
+      },
+      8: {
+        fontSize: '$8',
+      },
+      9: {
+        fontSize: '$9',
+      },
+      10: {
+        fontSize: '$10',
+      },
+      11: {
+        fontSize: '$11',
+      },
+      12: {
+        fontSize: '$12',
+      },
+    },
+    weight: {
+      1: {
+        fontWeight: '300'
+      },
+      2: {
+        fontWeight: '400'
+      },
+      3: {
+        fontWeight: '700'
+      },
+    },
+  },
 });
