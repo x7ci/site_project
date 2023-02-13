9b2717755dfdb40bf31b38cebb9df23b543b9770
x7ci <122831534+x7ci@users.noreply.github.com>
2023-02-13 21:12:33 +0100

    Chip: new component

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,59 @@
+import { type ReactNode } from 'react';
+import { styled } from 'stitches.config';
+import { Box, ColoredBox, Text } from '../stitches';
+
+interface Props {
+  children: ReactNode
+  text?: string
+  gap?: number
+  link?: string
+}
+
+const Chip = ({ children, text, link, gap = 8 }: Props) => {
+  return (
+    <Box
+      as="a"
+      href={link}
+      target="_blank"
+      css={{ all: 'unset' }}
+    >
+      <Wrapper>
+        <IconWrapper>
+          {children}
+        </IconWrapper>
+        <Box css={{ width: gap }} />
+        <ColoredBox color="gray2" size="unset" >
+          <Text size="5" color="cyanLight1" weight="3">{text}</Text>
+        </ColoredBox>
+      </Wrapper>
+    </Box>
+  );
+};
+
+const Wrapper = styled('button', {
+  border: 0,
+  fontFamily: 'inherit',
+  fontSize: 'inherit',
+  all: 'unset',
+  background: '$cyan10',
+  display: 'flex',
+  cursor: 'pointer',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  whiteSpace: 'nowrap',
+  padding: '3px 3px 3px 10px',
+
+});
+
+const IconWrapper = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  // background: 'red',
+  alignItems: 'center',
+  textAlign: 'center',
+  // margin: '0px 3px 0px 10px',
+});
+
+export default Chip;
