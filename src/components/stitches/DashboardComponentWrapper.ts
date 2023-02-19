50d2df1f905d18e6c9a256c8502023a6c5f40b7e
x7ci <122831534+x7ci@users.noreply.github.com>
2023-02-19 22:03:05 +0100

    DashboardComponentWrapper: new stitches element

---
+++ src/components/stitches/DashboardComponentWrapper.ts
@@ -0,0 +1,6 @@
+import { styled } from 'stitches.config';
+
+export const DashboardComponentWrapper = styled('div', {
+  height: 'calc($sizes$tileSize * 11)',
+  // overflow: 'hidden', // Use to check for overflowing components
+});
