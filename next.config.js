bb61be6626e1e71af270af870df441dad725819d
x7ci <122831534+x7ci@users.noreply.github.com>
2023-02-22 22:13:53 +0100

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["echarts", "zrender"]
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
+
+const nextConfig = {
+  assetPrefix,
+  basePath,
+  reactStrictMode: true,
+  transpilePackages: ['echarts', 'zrender']
+};
 
 module.exports = nextConfig;
