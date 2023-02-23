db763454781c7e4f24a0de6f5a312de6dec214c3
x7ci <122831534+x7ci@users.noreply.github.com>
2023-02-23 21:34:01 +0100

    fix config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = '';
-let basePath = '/';
+let basePath = '';
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
