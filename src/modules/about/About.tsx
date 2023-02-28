ffa1c224f141c5d06bc19d4c77c282c5cff88b5f
x7ci <122831534+x7ci@users.noreply.github.com>
2023-02-28 22:07:17 +0100

    update links

--- src/modules/about/About.tsx
+++ src/modules/about/About.tsx
@@ -1,7 +1,8 @@
 import DottedTopBorderBox from '@/components/DottedTopBorderBox';
-import UsernameIcon from '@/components/icons/UsernameIcon';
 import { Box, ColoredBox, Text } from '@/components/stitches';
 import { styled } from 'stitches.config';
+
+const githubRepoLink: string = `https://github.com/${process.env.GITHUB_REPOSITORY}`;
 
 const About = () => {
   return (
@@ -10,20 +11,12 @@ const About = () => {
         <Box css={{ display: 'flex', justifyContent: 'space-between' }}>
           <Box>
             <Text color="cyan1" weight={2}>ME </Text>
-            {/* <Text color="cyan1" weight={1} ></Text> */}
           </Box>
         </Box>
       </ColoredBox>
       <Box css={{ height: 15 }} />
       <DottedTopBorderBox>
         <Background>
-          {/* <Box css={{ height: 6 }} />
-          <UsernameWrapper>
-            <UsernameIcon width={160} height={70} scale={2.4} initialColor="cyan12" color="gray12" />
-          </UsernameWrapper>
-
-          <Box css={{ height: 6 }} /> */}
-
           <Box css={{ height: 10 }} />
           <Text color="gray12" size="6">
             This is my personal site.
@@ -37,7 +30,7 @@ const About = () => {
             <Text size="5" color="gray12" weight="3" background="cyan10" >
               <Box
                 as="a"
-                href="https://github.com/"
+                href={githubRepoLink}
                 target="_blank"
                 css={{ all: 'unset', cursor: 'pointer', padding: '0 4px 0 4px' }}
               >
@@ -45,24 +38,23 @@ const About = () => {
               </Box>
             </Text>
             .
-          </Text>
-          <Box css={{ height: 20 }} />
-          <Text>
-            You can contact me at
+            You can write to me at
             {' '}
             <Text size="5" color="gray12" weight="3" background="cyan10" >
               <Box
                 as="a"
-                href="mailto:mail@pm.me"
+                href="mailto:contact@x7ci.engineer"
                 target="_blank"
                 css={{ all: 'unset', cursor: 'pointer', padding: '0 4px 0 4px' }}
               >
-                mail@pm.me
+                contact@x7ci.engineer
               </Box>
             </Text>
             {' '}
             for inquiries.
           </Text>
+          {/* <Box css={{ height: 20 }} /> */}
+
         </Background>
       </DottedTopBorderBox>
     </Wrapper>
@@ -78,10 +70,6 @@ const Background = styled('div', {
   padding: '12px',
   display: 'flex',
   flexDirection: 'column'
-});
-
-const UsernameWrapper = styled('div', {
-  padding: '0px 12px 0px 12px',
 });
 
 export default About;
