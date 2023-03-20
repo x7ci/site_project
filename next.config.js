9e9fc78079d56c47f26f4db3eda083dbbfc9b5d5
x7ci <122831534+x7ci@users.noreply.github.com>
2023-03-20 21:03:43 +0100

    next.config.js: add comments

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const isGithubPages = false;
 
-let assetPrefix;
 let basePath;
+let assetPrefix;
 
+/** We need to set basePath and assetPrefix if hosted on github domain as it will be hosted on {user}.github.io/{repo} */
 if (isGithubActions && isGithubPages) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 
-  assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
+  assetPrefix = `/${repo}/`;
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix,
   basePath,
+  assetPrefix,
   reactStrictMode: true,
   transpilePackages: ['echarts', 'zrender'],
   images: {
