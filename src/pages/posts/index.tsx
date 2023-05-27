2428695190561a025070d90410d73337c99e938e
x7ci <122831534+x7ci@users.noreply.github.com>
2023-05-27 13:03:40 +0200

    Post list: change listStyleType

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Date from '@/modules/posts/date';
 import { getSortedPostsData, type PostDataSummary } from '@/lib/posts';
-import { ColoredBox, T } from '@/components/stitches';
+import { ColoredBox, T, T2 } from '@/components/stitches';
 import AnimateComponentMount from '@/components/animate-component-mount';
 import { styled } from 'stitches.config';
 
@@ -17,25 +17,24 @@ export default function Home({ allPostsData }: Props) {
         <title>x7ci | posts</title>
       </Head>
       <AnimateComponentMount>
-
         <ColoredBox color="cyan1">
           <T color="gray1" weight="2" >POSTS</T>
         </ColoredBox>
       </AnimateComponentMount>
 
-      <ul>
+      <List>
         {allPostsData?.map(({ markdownPath, date, title }, i) => (
           <AnimateComponentMount key={markdownPath} delay={0.03 * i}>
             <ListItem>
               <Link href={`/posts/${markdownPath}`} style={{ textDecoration: 'none' }}>
-                <T>{title}</T>
+                <T> {title}</T>
               </Link>
               <br />
               <Date dateString={date} />
             </ListItem>
           </AnimateComponentMount>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
@@ -49,6 +48,10 @@ export async function getStaticProps() {
     }
   };
 }
+
+const List = styled('ul', {
+  listStyleType: 'square',
+});
 
 const ListItem = styled('li', {
   padding: '0px 0 10px 0',
