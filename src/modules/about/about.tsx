6948919db7510af9a11f5a1e48ae5f7f27e1f41b
x7ci <122831534+x7ci@users.noreply.github.com>
2023-06-23 19:36:15 +0200

    update about page

--- src/modules/about/about.tsx
+++ src/modules/about/about.tsx
@@ -55,7 +55,20 @@ const About = () => {
                 </Box>
               </T>
             </Underline>
-            . Let's not miss a mutual opportunity.
+            . I'm currently open to work (
+            <Underline>
+              <T size="5" color="gray12" weight="3" background="cyan10">
+                <Box
+                  as="a"
+                  href="https://news.ycombinator.com/item?id=35424805#35585269"
+                  target="_blank"
+                  css={{ all: 'unset', cursor: 'pointer', padding: '0 4px 0 4px' }}
+                >
+                  HN entry
+                </Box>
+              </T>
+            </Underline>
+            ).
           </T>
         </Background>
       </CornerDotsBox>
