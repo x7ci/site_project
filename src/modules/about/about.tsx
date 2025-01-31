205d7a9d560b55eae61b1f5b64d45797c52a55a6
x7ci <122831534+x7ci@users.noreply.github.com>
2025-01-31 11:04:45 +0300

    updated text

--- src/modules/about/about.tsx
+++ src/modules/about/about.tsx
@@ -21,10 +21,10 @@ const About = () => {
           <T color="gray12" size="6">
             I'm a developer and Computer Science graduate with a strong background in frontend development, using primarily <b>React</b>, <b>TypeScript</b> and <b>Next.js</b>, and I have experience building large scale apps.
             I work across the stack and also have backend experience with <b>Postgres</b>, <b>GraphQL</b>, <b>Docker</b> and <b>C#</b> APIs.
-            I have 3+ years of experience working in the wind engineering industry as a Full Stack Developer, where I built <b>dashboards</b>, <b>charts</b> and <b>performant interfaces</b> coupled with unique designs and animations.
+            I have ~4 years of experience working in the wind engineering industry as a Software Developer, where I built <b>dashboards</b>, <b>charts</b> and <b>performant interfaces</b> coupled with unique designs and animations.
             <br />
             <br />
-            What you are looking at right now is my personal website - an over-engineered Next.js/React/TypeScript app with an FUI-inspired design system.
+            What you are looking at is an over-engineered Next.js/React/TypeScript app with an FUI-inspired design system.
             It features a dashboard reflecting the design system, using various components like tables, charts and metrics.
             All the data visualized by the components is locally generated without any external connections. Source code is available at&nbsp;
             <Underline>
@@ -42,7 +42,7 @@ const About = () => {
             .
             <br />
             <br />
-            I'm always open to collaborate - whether you're a recruiter or an independent hacker, please reach out to me at {' '}
+            Reach out to me at {' '}
             <Underline>
               <T size="5" color="gray12" weight="3" background="cyan10">
                 <Box
